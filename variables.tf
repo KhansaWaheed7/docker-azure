c944933b27553323b0639fb4388f3f17092408e7
Khansa Waheed <153375351+KhansaWaheed7@users.noreply.github.com>
2026-01-03 18:27:56 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "rg_name" {
+  description = "Azure Resource Group name"
+  type        = string
+  default     = "tfstaticwebsitestg-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
 variable "image_name" {
   description = "Docker image to deploy in Azure Container Instance"
   type        = string
